e1b8568ace6d7cba09134feb8c80b973f1e2d61a
tuongnguyen1209 <65478301+tuongnguyen1209@users.noreply.github.com>
2021-08-04 17:46:57 +0700

    fix height

--- src/RouterSales.js
+++ src/RouterSales.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import { WrapContent } from "./AppStyle";
 import Navigation from "./layout/Navigation/Navigation";
 import HomePage from "./pages/HomePage/HomePage";
 import Products from "./pages/ProductsPage/Products";
 
 const RouterSales = () => {
   return (
-    <>
+    <WrapContent>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/san-pham" component={Products} />
       </Switch>
       <Navigation />
-    </>
+    </WrapContent>
   );
 };
 
