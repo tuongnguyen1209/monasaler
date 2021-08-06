c8925087465c18eb093f8998ad30205c5425ba74
DuongNguyen <duongabc100@gmail.com>
2021-08-06 12:17:49 +0700

    add

--- src/apis/ProductApis.js
+++ src/apis/ProductApis.js
@@ -3,6 +3,10 @@ import axiosClient from "./AxiosClient";
 const url = "https://monacolor.herokuapp.com/api/v1/products";
 
 export const ProductApis = {
+  getAll: (params) => {
+    return axiosClient.get(url, { params });
+  },
+
   get: async (id) => {
     return await axiosClient.get(`${url}/${id}`);
   },
