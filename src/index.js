d54defd9db735536ea20638f887aac178ca26d5c
tuongnguyen1209 <65478301+tuongnguyen1209@users.noreply.github.com>
2021-09-21 23:47:31 +0700

    pwa ios

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import "reactjs-popup/dist/index.css";
 import "./index.css";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
-// import reportWebVitals from "./reportWebVitals";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -22,4 +22,4 @@ serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+reportWebVitals();
