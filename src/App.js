f21e298a046bc820dbc3d8f127bb0b2073d1d21d
tuongnguyen1209 <65478301+tuongnguyen1209@users.noreply.github.com>
2021-09-21 23:42:49 +0700

    pwa ios

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import { Container } from "./AppStyle";
 import UserProvider from "./contexts/UserContext";
+import PWAPrompt from "react-ios-pwa-prompt";
 import RouterSales from "./RouterSales";
 import NotificationContainer from "react-notifications/lib/NotificationContainer";
 
@@ -14,6 +15,13 @@ function App() {
           <Route path="/" component={RouterSales} strict />
         </UserProvider>
       </Container>
+      <PWAPrompt
+        promptOnVisit={3}
+        timesToShow={1}
+        copyClosePrompt="Close"
+        copyBody="This website has app functionality. Add it to your home screen to use it in fullscreen."
+        permanentlyHideOnDismiss={false}
+      />
     </BrowserRouter>
   );
 }
